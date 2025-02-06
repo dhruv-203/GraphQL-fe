59d78cd356ea6d10d0076be857191e595e93c6e5
dhruv-203 <pateldhruv2460@gmail.com>
2025-02-06 11:56:15 +0530

    Removed a silly mistake in the apollo client setup

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import AddTodo from "./pages/AddTodo.tsx";
 import Home from "./pages/Home.tsx";
 import UpdateTodo from "./pages/UpdateTodo.tsx";
 const client = new ApolloClient({
-  uri: `${import.meta.env.VITE_BACKEND_URL}/graphql"`,
+  uri: `${import.meta.env.VITE_BACKEND_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
